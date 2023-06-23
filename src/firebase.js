1a57b28c486eb9caba7c00a02d3276ca866fbacd
Mp-keyner <keyneroswaldo0@gmail.com>
2023-06-23 14:55:59 -0500

    23 de junio 2:55

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtzaoQy8QcKlijCb8UDMwUuciYGoVmuyo",
+  authDomain: "fb-picporter.firebaseapp.com",
+  projectId: "fb-picporter",
+  storageBucket: "fb-picporter.appspot.com",
+  messagingSenderId: "529999730318",
+  appId: "1:529999730318:web:072a4960415d54886a5bdf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
