6eb3e80180f29e9609cd2d0afdb5d7cd49cab996
Mp-keyner <keyneroswaldo0@gmail.com>
2023-07-06 15:30:42 -0500

    6 de julio 3:30 Primer commit despues de la verificacion de jesus

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,7 +4,8 @@ import "./index.css";
 import { Link, useNavigate } from "react-router-dom";
 import Salir from "../../img/salirIcon.svg";
 import { toast } from "react-toastify";
-// onClick={handleLogout}
+import usericon from "../../img/user.svg";
+
 const Nav = () => {
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
@@ -19,101 +20,89 @@ const Nav = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-  // const mensa = () => {
-  //   toast.success(
-  //     "Sé que quieres dejar ya todo esto, pero no te des por vencido tan pronto, tú puedes falta poco...Lo estás haciendo bien campeón🏆",
-  //     {
-  //       autoClose: 20000,
-  //     }
-  //   );
-  // };
+  console.log(window.screen.height - 56);
   return (
     <>
-      <div className="nav center">
+      <div className="nav center fondodef">
         <div>
-          <div className="center">
-            <img
-              src="https://raw.githubusercontent.com/Mp-keyner/PicPorter/f8a267d0f1c07e141187f68a4eda9a241b857af7/public/img/logo-ko.svg"
-              className="logo"
-              alt=""
-            />
-            <div className="center" style={{ gap: "0px" }}>
-              <h2>FB-PicPorter</h2>
+          <Link to={"/"}>
+            <div className="center" style={{ marginLeft: "2pc" }}>
               <img
-                src="https://raw.githubusercontent.com/Mp-keyner/PicPorter/f8a267d0f1c07e141187f68a4eda9a241b857af7/public/img/Pic.svg"
+                src="https://raw.githubusercontent.com/Mp-keyner/PicPorter/f8a267d0f1c07e141187f68a4eda9a241b857af7/public/img/logo-ko.svg"
+                className="logo"
                 alt=""
               />
+              <div className="center" style={{ gap: "0px" }}>
+                <h2>FB-PicPorter</h2>
+                <img
+                  src="https://raw.githubusercontent.com/Mp-keyner/PicPorter/f8a267d0f1c07e141187f68a4eda9a241b857af7/public/img/Pic.svg"
+                  alt=""
+                />
+              </div>
             </div>
-          </div>
+          </Link>
+        </div>
 
-          <div className="center">
-            {user.displayName == "Images" ? (
-              ""
+        <div
+          className="navMovil center"
+          style={{ top: isOpen ? "48px" : window.screen.height - 53 + "px" }}
+        >
+          <div className="menuN fondodef">
+            {isLoggedIn ? (
+              <div>
+                <ul className="center ulMovil">
+                  <Link to={"/Home"}>
+                    <li className="vertical">
+                      <img src={usericon} alt="User" />
+                      Home
+                    </li>
+                  </Link>
+                  <li>
+                    <div className="botonAdd">
+                      <div>+</div>
+                    </div>
+                  </li>
+                  <Link to={"/User"}>
+                    <li className="vertical">
+                      <img src={usericon} onClick={handleLogout} alt="User" />
+                      User
+                    </li>
+                  </Link>
+                </ul>
+              </div>
             ) : (
-              <>
-                <p>!HI¡</p>
-                <h2>{user.displayName}</h2>
-              </>
+              <></>
             )}
           </div>
         </div>
-        {isLoggedIn && (
-          <>
-            <div
-              className={`menu-toggle ${isOpen ? "open" : ""}`}
-              onClick={toggleMenu}
-            >
-              <span className="bar"></span>
-              <span className="bar"></span>
-              <span className="bar"></span>
-            </div>
-          </>
-        )}
-        <div
-          className={`center ${isLoggedIn ? "navMovil" : ""} ${
-            isOpen ? "show" : ""
-          }`}
-        >
+
+        <div className={`center show`}>
           {isLoggedIn ? (
             <div>
-              <ul className="center ulMovil">
+              <ul className="center ">
                 <Link to={"/Home"}>
                   <li onClick={toggleMenu}>Home</li>
                 </Link>
-                <li onClick={toggleMenu}>About</li>
-                <Link to={"/New"}>
-                  <li onClick={toggleMenu}>New Image</li>
-                </Link>
+                <li>
+                  <li>
+                    <div
+                      className="botonAdd"
+                      style={{
+                        top: isOpen ? "48px" : window.screen.height - 92 + "px",
+                      }}
+                    >
+                      <div>+</div>
+                    </div>
+                  </li>
+                  <Link to={"/"} onClick={logout}>
+                    <img src={usericon} alt="User" />
+                  </Link>
+                </li>
               </ul>
             </div>
           ) : (
             <></>
           )}
-          <div className="containerLogout">
-            {isLoggedIn ? (
-              <>
-                <li className="center">
-                  <img
-                    src={user.photoURL}
-                    alt="image user"
-                    className="imgUser"
-                  />
-                </li>
-                <li>
-                  <button className="cerrarSesion" onClick={handleLogout}>
-                    <img src={Salir} alt="image user" />
-                    Cerrar sesión
-                  </button>
-                </li>
-              </>
-            ) : (
-              <li>
-                <button className="iniciSesion" onClick={inicia}>
-                  Iniciar sesión
-                </button>
-              </li>
-            )}
-          </div>
         </div>
       </div>
     </>
@@ -121,3 +110,15 @@ const Nav = () => {
 };
 
 export default Nav;
+
+// {isLoggedIn && (
+//   <>
+//     <div
+//       className={`menu-toggle ${isOpen ? "open" : ""}`}
+//       onClick={toggleMenu}
+//     >
+//       <span className="bar"></span>
+//       <span className="bar"></span>
+//     </div>
+//   </>
+// )}
